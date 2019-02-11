ce53bae7a67e5b0dfb75d5124a331379fa5f9870
Sanika Dongre <sado9543@colorado.edu>
2019-02-10 20:56:38 -0700

    Update sortfunc.c

--- sortfunc.c
+++ sortfunc.c
@@ -6,10 +6,17 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <time.h>
+#include <string.h>
 
 int main()
 {    
-    int i, size =256, check; 
+   FILE* fptr;
+   int i, size =256, check; 
+   uint8_t* string_func = (uint8_t*)malloc(200);
+   pid_t proc_id = getpid();
+   uid_t user_id = getuid();
+   time_t t= time(NULL);
+   struct tm *tme_date = localtime(&t);
     time_t tcheck;
     int *buffer = (int*)malloc(sizeof(int)*size);
     int *buffer_sorted = (int*)malloc(sizeof(int)*size);
@@ -33,11 +40,23 @@ int main()
 		    printf("%d, ", buffer_sorted[i]);
 	    }
 	    printf("\n");
-	    printf("sorting gets done\n");		
-    }
-    else 
-    {
-         printf("System call has been failed %d\n", check);
-    }
-    return 0;
+	    printf("sorting gets done\n");	
+	    sprintf(string_func, "Processid=%d\n Userid=%d\n Timeanddate =%s\n Output sorted= %d, ", proc_id, user_id, asctime(tme_date), buffer_sorted);
+	    fptr = fopen("cron_log.txt","a");
+	    fwrite(string_func,1,strlen(string_func),fptr);
+	    for(i=0;i<size;i++)
+	    {
+		    sprintf(string_func,"%d ,", buffer_sorted[i]);
+		    fwrite(string_func,1,strlen(string_func),fptr);
+	    }
+	    printf("\n\n");
+	    fwrite(string_func,1,strlen(string_func),fptr);
+	    free(string_func);
+	    fclose(fptr);
+    	}
+    	else 
+    	{
+         	printf("System call has been failed %d\n", check);
+    	}
+    	return 0;
 }
